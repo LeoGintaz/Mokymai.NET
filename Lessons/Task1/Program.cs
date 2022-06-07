20d6d3f5166641dc88ddfe7bec969ae43a295575
Gintaras Leončikas <gintaras.leoncikas@codeacademylt.onmicrosoft.com>
2022-06-07 17:49:04 +0300

    nepavyko

--- Lessons/Task1/Program.cs
+++ Lessons/Task1/Program.cs
@@ -4,17 +4,35 @@
     {
         static void Main(string[] args)
         {
-           
+
             Console.WriteLine("Gintaras");
 
 
-            Console.WriteLine("-----Press any key to continue----------");
+            Console.WriteLine("--Press any key to clear and continue--");
             Console.ReadKey();
             Console.Clear();
 
             Console.WriteLine("Enter Name :");
             var letter = Console.ReadLine();
+
+            Console.WriteLine("Enter number :");
+            var number = Console.ReadLine();
+
+
+
+
             Console.WriteLine("First Letter of Name : " + letter[0] + "\n ASCII code for letter " + (int)letter[0]);
+            int x = letter[0];
+            int y = number[0];
+            int sum = x + y;
+            Console.WriteLine("Sum of ASCII and number = "+ (sum));
+
+
+
+
+
+
+
 
 
         }
