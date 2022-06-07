6888d9e1844a3c0facfb956b7ddc01348697f858
Gintaras Leončikas <gintaras.leoncikas@codeacademylt.onmicrosoft.com>
2022-06-07 16:47:19 +0300

    testas

--- Lessons/Task1/Program.cs
+++ Lessons/Task1/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! testas");
+            Console.WriteLine("Gintaras");
+
+       
         }
     }
 }
