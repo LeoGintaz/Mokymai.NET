fe1ca85ae1fc65f5a9f2aae51983ae912f9b2f6d
Gintaras Leončikas <gintaras.leoncikas@codeacademylt.onmicrosoft.com>
2022-06-07 22:55:21 +0300

    sss

--- Lessons/Task3Tower/Program.cs
+++ Lessons/Task3Tower/Program.cs
@@ -13,7 +13,7 @@
             string four = "    ***|***    ";
             string five = "   ****|****   ";
             string bottom = "-------";
-            //  Console.Write(one + Environment.NewLine + two + Environment.NewLine + three + Environment.NewLine + four + Environment.NewLine + five + Environment.NewLine + bottom + "1" + bottom);
+            //  Console.Write(one + Environment.NewLine + two + Environment.NewLine + three + Environment.NewLine + four + Environment.NewLine + five + Environment.NewLine + bottom + "1" + bottom); Testas Grazaus boksto
 
 
             Console.Write($"{one}{one}{one}\n{two}{one}{one}\n{three}{one}{one}\n{four}{one}{one}\n{five}{one}{one}\n{bottom}{"1"}{bottom}{bottom}{"2"}{bottom}{bottom}{"3"}{bottom}");
@@ -33,9 +33,39 @@
             five = "   ****|****   ";
             bottom = "-------";
 
-            Console.Write($"\n\n\n{five}{one}{one}\n{four}{one}{one}\n{three}{one}{one}\n{two}{one}{one}\n{one}{one}{one}\n{bottom}{"1"}{bottom}{bottom}{"2"}{bottom}{bottom}{"3"}{bottom}");
-         
+            Console.Write($"\n\n\n{one}{one}{one}\n{one}{one}{one}\n{one}{one}{one}\n{one}{one}{one}\n{five}{five}{five}\n{bottom}{"1"}{bottom}{bottom}{"2"}{bottom}{bottom}{"3"}{bottom}");
 
+            Console.Write($"\n\n\n" +
+                $"{one}{one}{one}" +
+                $"\n{one}{one}{one}" +
+                $"\n{one}{one}{one}" +
+                $"\n{one}{one}{two}" +
+                $"\n{five}{four}{three}" +
+                $"\n{bottom}{"1"}{bottom}{bottom}{"2"}{bottom}{bottom}{"3"}{bottom}");
+
+            Console.Write($"\n\n\n" +
+                $"{one}{one}{one}" +
+                $"\n{one}{one}{one}" +
+                $"\n{one}{one}{one}" +
+                $"\n{two}{one}{two}" +
+                $"\n{five}{four}{three}" +
+                $"\n{bottom}{"1"}{bottom}{bottom}{"2"}{bottom}{bottom}{"3"}{bottom}");
+
+            Console.Write($"\n\n\n" +
+                $"{one}{three}{one}" +
+                $"\n{one}{three}{one}" +
+                $"\n{one}{three}{one}" +
+                $"\n{two}{three}{two}" +
+                $"\n{five}{three}{three}" +
+                $"\n{bottom}{"1"}{bottom}{bottom}{"2"}{bottom}{bottom}{"3"}{bottom}");
+
+            Console.Write($"\n\n\n" +
+                $"{one}{one}{one}" +
+                $"\n{one}{one}{two}" +
+                $"\n{one}{one}{three}" +
+                $"\n{one}{one}{four}" +
+                $"\n{one}{one}{five}" +
+                $"\n{bottom}{"1"}{bottom}{bottom}{"2"}{bottom}{bottom}{"3"}{bottom}");
 
         }
     }
