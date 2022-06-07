7fb7efc8aff38d87c5995711aa84a37f682356ed
Gintaras Leončikas <gintaras.leoncikas@codeacademylt.onmicrosoft.com>
2022-06-07 17:04:09 +0300

    test

--- Lessons/Task1/Program.cs
+++ Lessons/Task1/Program.cs
@@ -4,9 +4,19 @@
     {
         static void Main(string[] args)
         {
+           
             Console.WriteLine("Gintaras");
 
-       
+
+            Console.WriteLine("-----Press any key to continue----------");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Enter Name :");
+            var letter = Console.ReadLine();
+            Console.WriteLine("First Letter of Name : " + letter[0] + "\n ASCII code for letter " + (int)letter[0]);
+
+
         }
     }
 }
