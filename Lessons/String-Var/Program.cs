5684ea5100d9211ea5cc83bceb2f845fb298fad4
Gintaras Leončikas <gintaras.leoncikas@codeacademylt.onmicrosoft.com>
2022-06-07 20:49:34 +0300

    kintamieji

---
+++ Lessons/String-Var/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace String_Var
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //variable types
+            //type  name
+            string kintamasis = "hello world"; //string
+            string tuscias = ""; //tuscias
+            string nulinis = null; //nieko nera
+            string laisvas = "          "; //empty space
+            var stringkint = "tekstas"; //bendras pagal auto priskiriama reiksme
+
+            string kontratinacija = kintamasis + stringkint;
+            string kompozicija = string.Format("{0}", kintamasis);
+            string interpoliacija = $"{laisvas}";
+
+            kintamasis = "tekstas belekoks"; // override of "kintamasis" 
+
+        }
+    }
+}
--- Lessons/Task2/Program.cs
+++ Lessons/Task2/Program.cs
@@ -4,12 +4,13 @@
     {
         static void Main(string[] args)
         {
-
+/*
             Console.WriteLine("Enter name: ");
             var name = Console.ReadLine();
             
             Console.WriteLine(Environment.NewLine + name + Environment.NewLine + name[1] + "    |     " + name + "|   " + name.Length);
-            
-        }
+            */
+
+          
     }
 }
