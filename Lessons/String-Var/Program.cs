c72ab8f0c44901bbfa0e2d36f90732a2f808222e
Gintaras Leončikas <gintaras.leoncikas@codeacademylt.onmicrosoft.com>
2022-06-08 19:39:48 +0300

    sss

--- Lessons/String-Var/Program.cs
+++ Lessons/String-Var/Program.cs
@@ -25,6 +25,33 @@
 
             kintamasis = "tekstas belekoks"; // override of "kintamasis" 
 
+
+
+            //From lesson variable types
+
+            //Sveikas skaičius
+            byte mazasSkaicius = 2; //galima priskirti nuo 0 iki 255
+            short trumpasSkaicius = 2; //max value 32767 min value -32768
+            long ilgasSkaicius = 2; //max ir min labai didelis, didesni negu int
+            int skaiciusss = 2; //max value 2147483647 min value -2147483647
+            
+
+            //tinka visiem skaiciu var. tipam
+
+            int maxIntSkaitmuo = int.MaxValue; // Išveda maksimalų galimą skaičių
+            int minIntSkaitmuo = int.MinValue; // Isveda minimalu galima skaiciu
+
+
+            //budai skaiciu var. priskirti null
+            int? skaiciusKurisGaliButNull;
+            skaiciusKurisGaliButNull = null;
+            Nullable<int> skaiciussKurisGaliButNull = null;
+            
+            
+            uint tikTeigiamasSkaicius = 2; //tik teigiami skaiciai max dvigubas lyginant su int/ushot/ulong
+
+
+
         }
     }
 }
