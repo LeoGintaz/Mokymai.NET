fe15060ce90e6e29be2e68e3a5dc6e4d6f5fc76f
Gintaras Leončikas <gintaras.leoncikas@codeacademylt.onmicrosoft.com>
2022-06-11 22:02:51 +0300

    a

--- Lessons/Task3Tower/Program.cs
+++ Lessons/Task3Tower/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Tower of Hanoi\n\n");
 
             
             string? zero = "       |       ";
