bf52d56384dacb14a2e07a5c3d891a75c5182907
Gintaras Leončikas <gintaras.leoncikas@codeacademylt.onmicrosoft.com>
2022-06-29 21:28:03 +0300

    comit

--- Lessons/Metodai!!!2/Program.cs
+++ Lessons/Metodai!!!2/Program.cs
@@ -4,53 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("metodai 2");
-            Console.WriteLine("iveskite teksta");
-            var tekstas = Console.ReadLine();
-            Console.WriteLine(Tarpai(tekstas));
-            //Console.WriteLine(TarpKiekis(tekstas));
-            //Console.WriteLine(TarpKiekisprad(tekstas));
-            //StarEndstring(tekstas);
+            /*  Console.WriteLine("metodai 2");
+              Console.WriteLine("iveskite teksta");
+              var tekstas = Console.ReadLine();
+              Console.WriteLine(Tarpai(tekstas));
+              //Console.WriteLine(TarpKiekis(tekstas));
+              //Console.WriteLine(TarpKiekisprad(tekstas));
+              //StarEndstring(tekstas);
+            */
+            string tekstas = Console.ReadLine();
+            Console.WriteLine(Mokausi(tekstas));
 
 
+          }/*
+          public static int Tarpai(string tarpai)
+          {
+              string pakitimas = tarpai.Replace(" ", "");
+              var atimtis = tarpai.Length - pakitimas.Length;
+              return atimtis;
+
+          }
+
+          public static int Word(string tarpai)
+          {
+              string[] split = tarpai.Split(' ');
+              var count = split.Count();
+              return count;
+          }
+          public static int TarpKiekis(string tarp)
+          {
+              var kiekis = tarp.Length - tarp.TrimEnd(' ').Length;
+              return kiekis;
+          }
+
+          public static int TarpKiekisprad(string tarp)
+          {
+              var kiekis = tarp.Length - tarp.TrimStart(' ').Length;
+              return kiekis;
+          }
+
+          public static int StarEndstring(string tekstas, out int tarpaiGale) 
+          {
+              tarpaiGale = tekstas.Length - tekstas.TrimEnd(' ').Length;
+              var kiekis2 = tekstas.Length - tekstas.TrimStart(' ').Length;
+              return kiekis2;
+
+          }*/
+
+
+        public static string Mokausi(string tekstas)
+        {
+            if (tekstas.Contains("mokausi"))
+                return "taip";
+            else return "ne";
+            
+            return tekstas;
 
         }
-        public static int Tarpai(string tarpai)
-        {
-            string pakitimas = tarpai.Replace(" ", "");
-            var atimtis = tarpai.Length - pakitimas.Length;
-            return atimtis;
-
-        }
- 
-        public static int Word(string tarpai)
-        {
-            string[] split = tarpai.Split(' ');
-            var count = split.Count();
-            return count;
-        }
-        public static int TarpKiekis(string tarp)
-        {
-            var kiekis = tarp.Length - tarp.TrimEnd(' ').Length;
-            return kiekis;
-        }
-
-        public static int TarpKiekisprad(string tarp)
-        {
-            var kiekis = tarp.Length - tarp.TrimStart(' ').Length;
-            return kiekis;
-        }
-
-        public static int StarEndstring(string tekstas, out int tarpaiGale) 
-        {
-            tarpaiGale = tekstas.Length - tekstas.TrimEnd(' ').Length;
-            var kiekis2 = tekstas.Length - tekstas.TrimStart(' ').Length;
-            return kiekis2;
-
-        }*/
-
-
-        
 
 
     }
--- Lessons/TestMetodai/UnitTest1.cs
+++ Lessons/TestMetodai/UnitTest1.cs
@@ -4,37 +4,14 @@ namespace TestMetodai2
     public class UnitTest1
     {
         [TestMethod]
-        public void Tarpai_Test1()
+        public void IsMokausi()
         {
             var fake = "as mokausi programuoti";
-            var expected = 2;
-            var actual = Metodai2.Program.Tarpai(fake);
+            var expected = "taip";
+            var actual = Metodai2.Program.Mokausi(fake);
             Assert.AreEqual(expected, actual);
         }
-        [TestMethod]
-        public void Tarpai_Test2()
-        {
-            var fake = " as mokausi programuoti ";
-            var expected = 4;
-            var actual = Metodai2.Program.Tarpai(fake);
-            Assert.AreEqual(expected, actual);
-        }
-        //-----------------------------------------------------
-        [TestMethod]
-        public void Tarpai_Test1()
-        {
-            var fake = "as mokausi programuoti";
-            var expected = 2;
-            var actual = Metodai2.Program.Tarpai(fake);
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void Tarpai_Test2()
-        {
-            var fake = " as mokausi programuoti ";
-            var expected = 4;
-            var actual = Metodai2.Program.Tarpai(fake);
-            Assert.AreEqual(expected, actual);
+        
         }
 
 
