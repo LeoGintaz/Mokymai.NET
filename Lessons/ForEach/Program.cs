e433fe314a270bd125adb5a4316946645a12875b
Gintaras Leon <leogintaz@gmail.com>
2022-07-21 20:01:11 +0300

    comit

---
+++ Lessons/ForEach/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+
+namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string seka = "1sd512sd5";
+            string sakinys = "vienas du trys keturi penki sesi septyni astuoni";
+            //IstraukSkaicius(seka);
+            //Rykiavimas();
+            IsmetytiZodzius(sakinys);
+           // Sujungimas(sakinys, "zodis po zodzio zodziuose");
+
+            /*  int[] taskuMasyvas = new int[10];
+              foreach (var item in taskuMasyvas)
+              {
+                  Console.WriteLine("Naujas elementas" + item);
+              }*/
+        }
+
+        public static void IsmetytiZodzius(string sakinys)
+        {
+            List<string> list = new List<string>(sakinys.Split(' '));
+            List<string> list2 = new List<string>();
+            List<string> list3 = new List<string>();
+            foreach (var item in list)
+            {
+                if (item.Length >= 5)
+                {
+                    list2.Add(item);
+                }
+                else list3.Add(item);
+            }
+            Console.WriteLine(String.Join(",", list3));
+            return list2;
+        }
+
+        public static void Sujungimas()
+        {
+            
+
+
+
+
+        }
+
+        /* public static void Rykiavimas(string sb)
+         {
+
+
+
+             for (int i = 0; i < sb.Length; i++)
+             {
+                 for (int j = i + 1; j < mas.Length; j++)
+                 {
+                     if (mas[i] > mas[j])
+                     {
+                         int temp = mas[i];
+                         mas[i] = mas[j];
+                         mas[j] = temp;
+                     }
+                 }
+             }
+
+
+             Console.WriteLine(String.Join(",", mas));
+             return mas;
+         }*/
+
+        public static string IstraukSkaicius(string seka)
+        {
+            var sb = new StringBuilder();
+            foreach (char i in seka)
+            {
+                if (char.IsNumber(i))
+                {
+                    sb.Append(i);
+                }
+                return sb.ToString();
+            }
+
+          return sb.ToString();
+        }
+    }
+}
+
+
+/*
+
+ 
+ 
+ 
+ */
---
+++ Lessons/kortos/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace kortos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tipai = "Sirdziu Bugnu Vynu Kryziu";
+            string kortos = "Tuzas Dviake Triake Keturake Penkiaake Seseake Septynake Astuonake Devynake Desimtake Bortukas Dama Karalius";
+            Kalade(tipai, kortos);
+        }
+
+        public static List<string> Kalade(string tipai, string kortos)
+        {
+            List<string> listtipai = new List<string>(tipai.Split(' '));
+            List<string> listkortos = new List<string>(kortos.Split(' '));
+            List<string> kalade = new List<string>();
+            for (int i = 0 ; i < listtipai.Count; i++)
+            {                
+                
+                var tipas = listtipai[i];
+                
+                foreach (var korta in listkortos)
+                {
+                    kalade.Add(tipas);
+                    kalade.Add($"{korta}\n");
+                }
+            }
+            Console.WriteLine(String.Join(" ", kalade));
+            return kalade;
+
+        }
+    }
+}
